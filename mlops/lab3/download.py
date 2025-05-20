04b00bd1b8181a772d62a6c2b2998da810ea32ae
olllleg <147877452+olllleg@users.noreply.github.com>
2025-05-20 18:17:11 +0500

    Update download.py

--- mlops/lab3/download.py
+++ mlops/lab3/download.py
@@ -2,46 +2,45 @@ import pandas as pd
 from sklearn.preprocessing import OrdinalEncoder
 
 def download_data():
-    df = pd.read_csv('https://raw.githubusercontent.com/dayekb/Basic_ML_Alg/main/cars_moldova_no_dup.csv', delimiter = ',')
-    df.to_csv("cars.csv", index = False)
+    #mydata
+    df = pd.read_csv('/home/oleg/mlops/International_Education_Costs.csv', delimiter = ',')
+    df.to_csv("cost.csv", index = False)
     return df
 
 def clear_data(path2df):
     df = pd.read_csv(path2df)
     
-    cat_columns = ['Make', 'Model', 'Style', 'Fuel_type', 'Transmission']
-    num_columns = ['Year', 'Distance', 'Engine_capacity(cm3)', 'Price(euro)']
+    cat_columns = ['Country', 'City', 'University', 'Program', 'Level']
+    num_columns = ['Duration_Years', 'Tuition_USD', 'Living_Cost_Index', 'Rent_USD','Visa_Fee_USD','Insurance_USD','Exchange_Rate']
     
-    question_dist = df[(df.Year <2021) & (df.Distance < 1100)]
-    df = df.drop(question_dist.index)
+    
+    df = df.dropna()
+    df = df.drop_duplicates()
     # Анализ и очистка данных
     # анализ гистограмм
-    question_dist = df[(df.Distance > 1e6)]
-    df = df.drop(question_dist.index)
     
     # здравый смысл
-    question_engine = df[df["Engine_capacity(cm3)"] < 200]
-    df = df.drop(question_engine.index)
+    question_duration = df[df["Duration_Years"] > 8]
+    df = df.drop(question_duration.index)
     
     # здравый смысл
-    question_engine = df[df["Engine_capacity(cm3)"] > 5000]
-    df = df.drop(question_engine.index)
+    question_index = df[df["Living_Cost_Index"] > 100]
+    df = df.drop(question_index.index)
     
     # здравый смысл
-    question_price = df[(df["Price(euro)"] < 101)]
-    df = df.drop(question_price.index)
-    
-    # анализ гистограмм
-    question_price = df[df["Price(euro)"] > 1e5]
+    question_price = df[df["Tuition_USD"] < 10000]
     df = df.drop(question_price.index)
     
     #анализ гистограмм
-    question_year = df[df.Year < 1971]
-    df = df.drop(question_year.index)
+    question_visa = df[df['Visa_Fee_USD'] > 1000]
+    df = df.drop(question_visa.index)
     
-    df = df.reset_index(drop=True)  
+    df = df.reset_index(drop=True)  # обновим индексы в датафрейме DF. если бы мы прописали drop = False, то была бы еще одна колонка - старые индексы
+   
+    # Порядковое кодирование. Обучение, трансформация и упаковка в df
+    
     ordinal = OrdinalEncoder()
-    ordinal.fit(df[cat_columns])
+    ordinal.fit(df[cat_columns]);
     Ordinal_encoded = ordinal.transform(df[cat_columns])
     df_ordinal = pd.DataFrame(Ordinal_encoded, columns=cat_columns)
     df[cat_columns] = df_ordinal[cat_columns]
@@ -49,4 +48,4 @@ def clear_data(path2df):
     return True
 
 download_data()
-clear_data("cars.csv")
+clear_data("cost_csv.csv")
