4a9e4b4dd73b723ee8ec8d58c5a39b7886b1a283
olllleg <147877452+olllleg@users.noreply.github.com>
2025-05-20 21:19:03 +0500

    Update download.py

--- mlops/lab3/download.py
+++ mlops/lab3/download.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import OrdinalEncoder
 
 def download_data():
     #mydata
-    df = pd.read_csv('/var/lib/jenkins/workspace/download/mlops/lab3',  delimiter=',')
+    df = pd.read_csv('/var/lib/jenkins/workspace/download/mlops/lab3/International_Education_Costs.csv',  delimiter=',')
     df.to_csv("cost.csv", index = False)
     return df
 
